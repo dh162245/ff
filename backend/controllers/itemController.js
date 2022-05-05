5875f5f29919b43c299007ffb6720c7713b8b9a1
dh162245 <daniel.huck@hotmail.co.uk>
2022-05-05 06:01:49 +0200

    rest api

--- backend/controllers/itemController.js
+++ backend/controllers/itemController.js
@@ -1,11 +1,14 @@
 const asyncHandler = require('express-async-handler')
+const Item = require('../models/itemModel')
 
 
 // @desc Get items
 // @route GET api/items
 // @access Private
 const getItem = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Get items'})
+    const items = await Item.find()
+
+    res.status(200).json({ items })
 })
 
 // @desc Edit items
@@ -16,21 +19,44 @@ const createItem = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error('add a text')
     }
-   res.status(200).json({ message: 'Create items'})
+
+   const item = await Item.create({
+       text: req.body.text
+   }) 
+    
+   res.status(200).json({ item })
 })
 
 // @desc Edit items
 // @route PUT api/items/:id
 // @access Private
 const editItem = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Edit item ${req.params.id}`})
+    const item = await Item.findById(req.params.id)
+
+    if(!item){
+        res.status(400)
+        throw new Error('Item not found')
+    }
+
+    const editedItem = await Item.findByIdAndUpdate(req.params.id, req.body, {new: true})
+
+    res.status(200).json({ editedItem })
 })
 
 // @desc Get items
 // @route GET api/items
 // @access Private
 const deleteItem = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Delete item ${req.params.id}`})
+    const item = await Item.findById(req.params.id)
+
+    if(!item){
+        res.status(400)
+        throw new Error('Item not found')
+    }
+
+    await item.remove()
+
+    res.status(200).json({ id: req.params.id })
 })
 
 module.exports = {
